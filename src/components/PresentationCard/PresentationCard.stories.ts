87664eadb558e7cf6df3dbc8ae3e9049cde0c287
IngDavidTM <david-1011@hotmail.com>
2024-05-06 11:24:49 -0500

    Add presentation Card component

---
+++ src/components/PresentationCard/PresentationCard.stories.ts
@@ -0,0 +1,34 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { PresentationCard } from './PresentationCard';
+
+const meta: Meta<typeof PresentationCard> = {
+  title: 'Components/Presentation Card',
+  component: PresentationCard,
+  parameters: {
+    layout: 'centered',
+  },
+  argTypes: {
+    imageUrl: {
+      control: 'text',
+      defaultValue: 'https://www.pngall.com/wp-content/uploads/8/Work-PNG-File-Download-Free.png',
+    },
+    company: {
+      control: 'text',
+      defaultValue: 'tagline',
+    },
+    tagline: {
+      control: 'text',
+      defaultValue: 'tagline',
+    },
+  },
+};
+
+export default meta;
+
+export const LogIn: StoryObj<typeof PresentationCard> = {
+  args: {
+    imageUrl: 'https://www.pngall.com/wp-content/uploads/8/Work-PNG-File-Download-Free.png',
+    company: 'Unity Work Desk',
+    tagline: 'Streamline all personnel tasks with ease',
+  }
+};
---
+++ src/components/PresentationCard/PresentationCard.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Image from "next/image";
+import Text from "../Text/Text";
+
+interface PresentationCardProps {
+  company: string;
+  tagline: string;
+  imageUrl: string;
+}
+
+export const PresentationCard: React.FC<PresentationCardProps> = ({
+  company,
+  tagline,
+  imageUrl,
+}) => {
+  return (
+    <article className="flex flex-col bg-gray-200 w-screen h-screen justify-center items-center gap-16" >
+      <Image
+        src={imageUrl}
+        alt="Working desk"
+        width={400}
+        height={400}
+      />
+      <div className="flex flex-col gap-4">
+        <Text variant="h1" className="font-bold text-center">
+          {company}
+        </Text>
+        <Text variant="p" className="text-center">
+          {tagline}
+        </Text>
+      </div>
+    </article>
+  );
+};
